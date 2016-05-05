bd0aa11bfd86bf307e01533f5d2aab72351bbc8b
Iran Reyes Fleitas <iranreyesfleitas@gmail.com>
2016-05-05 17:36:07 -0300

    Make the lib lite

--- base.js
+++ base.js
@@ -1,4 +1,4 @@
-var TweenMax = require('gsap')
+var TweenLite = require('gsap/src/uncompressed/TweenLite');
 var assign = require('object-assign')
 
 module.exports = function(Promise) {
@@ -19,17 +19,17 @@ module.exports = function(Promise) {
     })
   }
 
-  var animateTo = animateFunc.bind(null, TweenMax.to)
+  var animateTo = animateFunc.bind(null, TweenLite.to)
 
   var util = animateTo
   util.to = animateTo
-  util.from = animateFunc.bind(null, TweenMax.from)
+  util.from = animateFunc.bind(null, TweenLite.from)
 
   util.set = function animateSet(element, params) {
     params = assign({}, params)
     return new Promise(function(resolve, reject) {
       params.onComplete = resolve
-      TweenMax.set(element, params)
+      TweenLite.set(element, params)
     })
   }
 
@@ -38,36 +38,12 @@ module.exports = function(Promise) {
     var tween
     return new Promise(function(resolve, reject, onCancel) {
       to.onComplete = resolve
-      tween = TweenMax.fromTo(element, duration, from, to)
+      tween = TweenLite.fromTo(element, duration, from, to)
       onCancel && onCancel(function(){
         tween.kill()
       })
     })
   }
-
-  ;['staggerTo', 'staggerFrom'].forEach(function(fn) {
-    var tweenFunc = TweenMax[fn]
-    var tweens
-    util[fn] = function(element, duration, from, stagger) {
-      return new Promise(function(resolve, reject, onCancel) {
-        tweens = tweenFunc(element, duration, from, stagger, resolve)
-        onCancel && onCancel(function(){
-          tweens.forEach( function (tween) { tween.kill() })
-        })
-      })
-    }
-  })
-
-  util.staggerFromTo = function staggerFromTo(element, duration, from, to, stagger, position) {
-    var tweens
-    return new Promise(function(resolve, reject, onCancel) {
-      tweens = TweenMax.staggerFromTo(element, duration, from, to, stagger, resolve)
-      onCancel && onCancel(function(){
-        tweens.forEach( function (tween) { tween.kill() })
-      })
-    })
-  }
-
 
   util.all = Promise.all
   return util
