3451ab6bb623411c41b2fea2b9959b457db24f4d
Iran Reyes <iran.reyes@jam3.com>
2018-02-26 00:47:52 -0500

    Update GSAP, add cssplugin

--- base.js
+++ base.js
@@ -1,47 +1,50 @@
-require('gsap/src/uncompressed/TweenLite');
-var assign = require('object-assign')
+require('gsap/src/minified/plugins/CSSPlugin.min.js');
+require('gsap/src/minified/TweenLite.min.js');
+var assign = require('object-assign');
 
 module.exports = function(Promise) {
   function animateFunc(func, element, duration, opts) {
-    opts = assign({}, opts)
-    var tween
+    opts = assign({}, opts);
+    var tween;
     return new Promise(function(resolve, reject, onCancel) {
-      opts.onComplete = resolve
-      tween = func(element, duration, opts)
-      onCancel && onCancel(function(){
-        tween.kill()
-      })
-    })
+      opts.onComplete = resolve;
+      tween = func(element, duration, opts);
+      onCancel &&
+        onCancel(function() {
+          tween.kill();
+        });
+    });
   }
 
-  var animateTo = animateFunc.bind(null, TweenLite.to)
+  var animateTo = animateFunc.bind(null, TweenLite.to);
 
-  var util = animateTo
-  util.to = animateTo
-  util.from = animateFunc.bind(null, TweenLite.from)
+  var util = animateTo;
+  util.to = animateTo;
+  util.from = animateFunc.bind(null, TweenLite.from);
 
   util.set = function animateSet(element, params) {
-    params = assign({}, params)
+    params = assign({}, params);
     return new Promise(function(resolve, reject) {
-      params.onComplete = resolve
-      TweenLite.set(element, params)
-    })
-  }
+      params.onComplete = resolve;
+      TweenLite.set(element, params);
+    });
+  };
 
   util.fromTo = function animateFromTo(element, duration, from, to) {
-    to = assign({}, to)
-    var tween
+    to = assign({}, to);
+    var tween;
     return new Promise(function(resolve, reject, onCancel) {
-      to.onComplete = resolve
-      tween = TweenLite.fromTo(element, duration, from, to)
-      onCancel && onCancel(function(){
-        tween.kill()
-      })
-    })
-  }
+      to.onComplete = resolve;
+      tween = TweenLite.fromTo(element, duration, from, to);
+      onCancel &&
+        onCancel(function() {
+          tween.kill();
+        });
+    });
+  };
 
   util.killTweensOf = TweenLite.killTweensOf.bind(TweenLite);
   util.all = Promise.all;
   util.TweenLite = TweenLite;
-  return util
-}
+  return util;
+};
